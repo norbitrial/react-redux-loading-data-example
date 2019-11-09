2801443c79a5a4af729fd944e0e2b8a55313506e
unknown <norbitrial8@gmail.com>
2019-11-09 22:11:31 +0100

    Fake data load working with loading indicator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import Console from './Components/Console';
 import Loader from './Components/Loader';
+import types from './Duck/FakeData/types';
 import './App.css';
 
 function App() {
   const fakeDataReducer = useSelector(state => state.fakeDataReducer);
-  
+  const dispatch = useDispatch();
+
+  const hasToSucceed = () => {
+    return Math.floor(Math.random() * Math.floor(2)) === 1;
+  };
+
+  const getFakeData = () => {
+    dispatch({type: types.DATA_FETCH_STARTED});
+    
+    const type = hasToSucceed() ? types.DATA_FETCH_SUCCESS: types.DATA_FETCH_FAILED;
+
+    setTimeout(() => {
+      dispatch({type: type});
+      dispatch({type: types.DATA_FETCH_FINISHED});
+    }, 2000);
+  };
+
   return (
     <div className="container">
       <div className="item">
         <Console />
       </div>
       <div className="item">
-        <button>Click here to simulate data load</button>
+        <button onClick={() => { getFakeData() }}>Click here to simulate data load</button>
 
         {
           fakeDataReducer.isLoading ?
--- src/Duck/FakeData/types.js
+++ src/Duck/FakeData/types.js
@@ -2,6 +2,7 @@ const types = {
     DATA_FETCH_STARTED: 'fake-data/data-fetch-started',
     DATA_FETCH_SUCCESS: 'fake-data/data-fetch-success',
     DATA_FETCH_FAILED: 'fake-data/data-fetch-failed',
+    DATA_FETCH_FINISHED: 'fake-data/data-fetch-finished',
 };
 
 export default types;
