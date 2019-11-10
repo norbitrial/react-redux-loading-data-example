b15a333dc8ae3b6cef1b55b5e1a9d5eb1aef7232
unknown <norbitrial8@gmail.com>
2019-11-10 11:52:38 +0100

    Corrected unit test for app render

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureMockStore from "redux-mock-store";
 import App from './App';
+
+const fakeDataReducer = {
+  isLoading: false,
+  items: [],
+  errorMessage: null,
+};
+const mockStore = configureMockStore();
+const store = mockStore({fakeDataReducer});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  
+  ReactDOM.render(<Provider store={store}>
+                    <App  />
+                  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
